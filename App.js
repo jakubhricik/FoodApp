56fb4499ef7e5c263762c5bb9f3e86ad8d1d2c71
Jakub Hricik <jh134yc@gmail.com>
2021-07-04 20:27:08 +0200

    setting up fonts

--- App.js
+++ App.js
@@ -3,11 +3,24 @@ import { Login, Recipe } from "./screens";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+//fonts
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
