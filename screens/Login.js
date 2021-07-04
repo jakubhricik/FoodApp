119e93a57d9422a32ad2ac23514b31a55646ac91
Jakub Hricik <jh134yc@gmail.com>
2021-07-04 20:21:30 +0200

    setting up assets

---
+++ screens/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+
+const Login = ({ navigation }) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Login;
